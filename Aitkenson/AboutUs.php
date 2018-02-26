66126e8a29ad7c410b51f0d8f74d4fe3dab9e4aa
ibfailing <huikonrad@gmail.com>
2018-02-26 10:18:37 +0800

    oh dear
    beaver

---
+++ Aitkenson/AboutUs.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="w3.css">
+<title>About Us</title>
+</head>
+<body>
+	<header class="w3-red">
+	<img src="header.png" alt="aitkenson logo no background" class="w3-red"><br>
+		<nav class="w3-red floatright">
+		<a href="Home.php">Home</a>
+		<a href="shop.php">Shop</a>
+		<a href="AboutUs.php">About Us</a>
+		<a href="contactus.php">Contact Us</a>
+		<a href="search.php">Search</a>
+	</nav><br>
+	</header>
+	<div class="left">
+		<h1>
+			
+		</h1>
+	</div>
+	<div class="center">
+	<h1>
+	About Us
+	</h1>
+	<p1>
+		Aitkenson International (Far East) Ltd. is one of the worldwide leaders in audio visual products including clock radios, DSP radios, portable radios, MP3/ MP4 players, speakers and other AV products. Aitkenson was founded in 1985 with the vision that multimedia would revolutionize the way people interact with their audio products.
+	</p1><br><br>
+	<p2>
+		Famous for its digital entertainment audio products and for launching the multimedia revolution, Aitkenson is now driving digital entertainment with products like its highly acclaimed MP3/ MP4 players. The company's innovative hardware, proprietary technology, applications and services leverage the Internet enabling cons umers to experience high quality digital entertainment anytime, anywhere.
+	</p2><br><br>
+	<p3>
+		Since launching the phenomenally successful audio products from 1989 onwards, Aitkenson has been able to leverage on its cutting edge audio technology, huge user base and strong brand name to expand into the exciting lifestyle Personal Digital Entertainment market.
+	</p3><br><br>
+	<p4>
+		Today, Aitkenson is widely recognized as a global leader for product innovation in the audio and PDE segments, offering a complete, high quality digital entertainment experience through the company’s hardware and services. Finding the perfect balance between form and function, Aitkenson strives for making every product a sensory feast for the eyes and ears.
+	</p4><br><br>
+	<p5>
+		These products are marketed to consumers and system integrators through a worldwide distribution network including traditional marketing channels, original equipment manufacturers (OEMs) and the Internet.
+	</p5><br><br>
+	<p6>
+		Furthermore, Aitkenson has forged partnerships with some of the industry's top technology companies and developers worldwide, fortifying our strength and leadership in product innovation and research & development.
+	</p6><br><br>
+	<p7>
+		Moving forward, Aitkenson's mission is to expand its leadership role in the lifestyle Personal Digital Entertainment market by using innovative technology, broadband and leading-edge designs for not only the technically progressive consumers, but also for everyone who enjoys digital entertainment
+	</p7><br><br>
+	<p8>
+		With strong focus on user friendly interface, multiple features and excellent industrial designs of it’s products, the Aitkenson brand is synonymous with lifestyle Personal Digital Entertainment.
+	</p8><br><br>
+	<p9>
+		To meet the increasing demand of satisfying customer's requirements, in 2007, Aitkenson has expanded its production capacities and product development in Zhongshan, China. The new office shows the company’s commitment and dedication to serve each and every customer's requirement in the future.
+	</p9><br><br>
+	</div>
+</body>
+</html>
---
+++ Aitkenson/Home.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width = device-width, initial scale =1">
+
+<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+<title>Untitled Document</title>
+	<title>Aitkenson - Home</title>
+</head>
+<body>
+	<div class="container">
+	<div class="page-header">
+			<h1 class="bg-danger text-white"><a href="Home.php"><img src="header1.png"></a>
+			</h1>
+			<a href="Home.php">Home</a>
+			<a href="shop.php">Shop</a>
+			<a href="AboutUs.php">About Us</a>
+			<a href="contactus.php">Contact Us</a>
+			<a href="search.php">Search</a>
+	</div>
+	<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+    <!-- Carousel indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>   
+    <!-- Carousel items -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="P.1 - 5 clock radio/CR-323P.jpg" alt="First Slide">
+            <div class="carousel-caption">
+                <h3>First slide label</h3>
+                <p>Lorem ipsum dolor sit amet...</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="P.10-11 bluetooth speaker/BT-156.jpg" alt="Second Slide">
+            <div class="carousel-caption">
+                <h3>Second slide label</h3>
+                <p>Aliquam sit amet gravida nibh, facilisis...</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="header.jpg" alt="Third Slide">
+            <div class="carousel-caption">
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna vel...</p>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</div>
+
+	<div class="row">
+	  <div class="col-xs-4 text-center">
+	  <button type="button" class="btn btn-default">
+			<p>
+			Products
+		  </p></button>
+	  </div>
+	  <div class="col-xs-4 text-center">
+			<button type="button" class="btn btn-default ">
+			<p>
+			New Arrivals
+				</p></button>
+	  </div>
+	  <div class="col-xs-4 text-center">
+	  <button type="button" class="btn btn-default">
+			<p>
+				Enquiries
+		  </p></button>
+	  </div>
+	</div>
+	</div>
+	
+	
+	  <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</body>
+</html>
